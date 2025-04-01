829d252233afc24dd366db3ef1f5440e86fb7dce
unknown <seifeddine.elkorichi@hotmail.com>
2025-04-01 18:47:53 +0100

    main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Dashboard from '@/app/dashboard/page'
+
 const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h2 className='text-2xl'>Hello Pinazza</h2>
+      <Dashboard />
     </main>
   )
 }
