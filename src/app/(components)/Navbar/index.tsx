288dec0579cf76f15f67eee29c76b37e316d58df
unknown <seifeddine.elkorichi@hotmail.com>
2025-04-05 19:00:29 +0100

    creating Menu button for the Navbar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import { Menu } from 'lucide-react'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='flex justify-between items-center w-full mb-7'>
+      {/* LEFT SIDE */}
+      <div className='flex justify-between items-center gap-5'>
+        <button
+          className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
+          onClick={() => {}}
+        >
+          <Menu className='w-4 h-4' />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
